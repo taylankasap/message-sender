502f75111736aba823d8b9c93bd41943e26cd55e
Taylan Kasap <taylankasap@gmail.com>
2025-05-30 20:41:33 +0300

    test: add name for db.New testcase

--- db/db_test.go
+++ db/db_test.go
@@ -8,20 +8,22 @@ import (
 	"github.com/taylankasap/message-sender/db"
 )
 
-func TestNewDatabase(t *testing.T) {
-	testFile := "test_db.sqlite3"
-	_ = os.Remove(testFile)
+func TestNew(t *testing.T) {
+	t.Run("it should create a database and the 'message' table", func(tt *testing.T) {
+		testFile := "test_db.sqlite3"
+		_ = os.Remove(testFile)
 
-	database, err := db.New(&db.Config{Filename: testFile})
-	require.NoError(t, err)
-	require.NotNil(t, database.Conn)
+		database, err := db.New(&db.Config{Filename: testFile})
+		require.NoError(tt, err)
+		require.NotNil(tt, database.Conn)
 
-	row := database.Conn.QueryRow("SELECT name FROM sqlite_master WHERE type='table' AND name='message'")
-	var tableName string
-	err = row.Scan(&tableName)
-	require.NoError(t, err)
-	require.Equal(t, "message", tableName)
+		row := database.Conn.QueryRow("SELECT name FROM sqlite_master WHERE type='table' AND name='message'")
+		var tableName string
+		err = row.Scan(&tableName)
+		require.NoError(tt, err)
+		require.Equal(tt, "message", tableName)
 
-	database.Conn.Close()
-	_ = os.Remove(testFile)
+		database.Conn.Close()
+		_ = os.Remove(testFile)
+	})
 }
